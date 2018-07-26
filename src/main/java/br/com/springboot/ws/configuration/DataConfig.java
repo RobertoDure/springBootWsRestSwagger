9dd08eef580b7c2dafe3725c2259832d8df5874f
Roberto Duré <roberto.dure@ideal-sumario.pt>
2018-07-26 16:57:44 +0100

    Update database driver

--- src/main/java/br/com/springboot/ws/configuration/DataConfig.java
+++ src/main/java/br/com/springboot/ws/configuration/DataConfig.java
@@ -12,17 +12,22 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfig {
 
+	/*
+	MariaDB Driver include
+	 */
 	@Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/match");
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/match");
         dataSource.setUsername("root");
         dataSource.setPassword("root");
         return dataSource;
     }
 	
-
+	/*
+	MariaDB Dialect
+	 */
 
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
@@ -30,7 +35,7 @@ public class DataConfig {
 		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDB53Dialect");
 		adapter.setPrepareConnection(true);
 		return adapter;
 	}
